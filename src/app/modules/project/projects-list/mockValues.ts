a37bf814866bdf965a1d4154552ba293cc11cb8c
rumabe97 <rumabe97@gmail.com>
2024-01-28 14:01:25 +0100

    Add about me

--- src/app/modules/project/projects-list/mockValues.ts
+++ src/app/modules/project/projects-list/mockValues.ts
@@ -3,7 +3,7 @@ import {IProject} from "../IProject";
 export const projects: IProject[] = [
     {
         name: 'Project 1',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut nisl a ante tincidunt tincidunt vel ac nisl. Donec at ante turpis. Phasellus ligula ipsum, scelerisque eu pellentesque ac, rutrum in elit. Vestibulum sapien mauris, auctor sit amet purus sed, dignissim pretium erat. Proin ultricies accumsan ipsum, et scelerisque ipsum laoreet sit amet. Ut et dui ante. Aliquam mattis leo ac dolor faucibus pulvinar. Curabitur bibendum lorem diam, at porttitor mi ornare quis. Praesent tincidunt elementum turpis, et rutrum tortor imperdiet a. Duis facilisis gravida urna sed sodales. Etiam eget fermentum risus, sed egestas risus. Aliquam mattis lorem vel dapibus mattis. Nunc condimentum arcu vel ipsum porttitor, et gravida purus rhoncus.',
+        description: 'A digital calendar enables effortless addition and editing of events. By setting event times, an alarm can be activated to receive timely notifications. This tool organizes your schedule, ensuring you never miss an important appointment by reminding you in advance of the exact timing of each scheduled event.',
         urlLive: 'www.google.com',
         urlGit: 'www.github.com',
         image: ''
