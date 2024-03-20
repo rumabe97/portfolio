1c3c824cfbba3c746952cd6201dbb8ef26a997ef
rumabe97 <rumabe97@gmail.com>
2024-03-20 16:18:17 +0100

    Add ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -22,7 +22,9 @@ import {NgxScrollrevealModule} from "./ngx-scrollreveal/ngx-scrollreveal.module"
         NgxScrollrevealModule,
         FontAwesomeModule
     ],
-    providers: [],
+    providers: [
+    provideClientHydration()
+  ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
