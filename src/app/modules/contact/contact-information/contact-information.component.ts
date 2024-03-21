9d03070cdf5814270b5311ef201e23f48f33b1f7
rumabe97 <rumabe97@gmail.com>
2024-03-21 17:46:23 +0100

    Fix links and up icon

--- src/app/modules/contact/contact-information/contact-information.component.ts
+++ src/app/modules/contact/contact-information/contact-information.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {NgsRevealConfig} from "../../../ngx-scrollreveal/services";
 import {ScrollConfigService} from "../../../core/services/scroll/scroll-config.service";
 import { faLinkedinIn, faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact-information',
@@ -12,6 +13,7 @@ export class ContactInformationComponent implements OnInit {
   faLinkedin = faLinkedinIn;
   faGithub = faGithub;
   faGMail = faGoogle;
+  faAngleUp = faAngleUp;
   constructor(private scrollService: ScrollConfigService) { }
 
   ngOnInit(): void {
