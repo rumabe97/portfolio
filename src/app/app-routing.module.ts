2a679232078cdb7f724d7053b2ee1a60fb43329e
rumabe97 <rumabe97@gmail.com>
2024-03-22 10:47:35 +0100

    Fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ComingSoonComponent} from "./shared/components/coming-soon/coming-soon.component";
 import {MainLayoutComponent} from "./core/main-layout/main-layout.component";
 
 const routes: Routes = [
@@ -8,6 +7,8 @@ const routes: Routes = [
         path: '',
         component: MainLayoutComponent,
     },
+    {path: '**', redirectTo: '/'},
+
 ];
 
 @NgModule({
